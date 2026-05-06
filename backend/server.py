4b0ad73f1a9a17e1160799225240e011a2707408
harshithaammu357 <harshithaammu357@gmail.com>
2026-05-06 20:11:46 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -508,7 +508,7 @@ app.include_router(api)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://your-vercel-url.vercel.app", "http://localhost:3000"],
+    allow_origins=["*", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
